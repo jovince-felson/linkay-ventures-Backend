5c8ca5fb3351655b9aa7703b8df5dbfd633e214b
Maria-Jovince <150923260+jovince-felson@users.noreply.github.com>
2026-05-06 11:19:35 +0530

    cha

--- services/tokenization-service/src/models/index.js
+++
@@ -1,5 +0,0 @@
-import TokenizationJob from "./tokenization.job.model.js";
-import ComplianceRule from "./compliance.rule.model.js";
-import AuditLog from "./audit.log.model.js";
-
-export { TokenizationJob, ComplianceRule, AuditLog };
